a055f6bf843b971c8e277128458d54dd7dc80300
nouveau-riche <2019kucp1049@iiitkota.ac.in>
2021-03-06 16:24:26 +0530

    removing agro : concept of video calling

--- lib/agora/util/settings.dart
+++
@@ -1,4 +0,0 @@
-/// Define App ID and Token
-const APP_ID = '6094567fffa24c9a93d98fd9cabc7b48';
-const Token =
-    '0066094567fffa24c9a93d98fd9cabc7b48IADMIQ77dfaUCRDMeTf14EBJ8S/q553pm7Oe3sFXGhPvoFXLtvgAAAAAEAAlICC+4YgBYAEAAQDgiAFg';
